faff948bfe627238240ce77779547ea776b98988
Sourav Kulkarni <souruly@gmail.com>
2020-10-08 14:12:25 +0530

    Fixed double click problem

--- sketch.js
+++ sketch.js
@@ -3,19 +3,21 @@ let lights = [];
 let states = [true, false];
 let center;
 let initialStates = [];
+let lastClickFrameCOount = 0;
 
 function setup() {
   createCanvas(600, 600);
   angleMode(DEGREES);
+  frameRate(15);
   center = createVector(300, 300);
 
-  numberOfLights = round(random(3,15));
+  numberOfLights = round(random(3, 15));
 
   let radius = createVector(200, 0);
   for (let i = 0; i < numberOfLights; i++) {
     let position = p5.Vector.add(center, radius);
-    let state = random(states)
-    let l = new Light(position.x, position.y, state, i+1);
+    let state = random(states);
+    let l = new Light(position.x, position.y, state, i + 1);
     lights.push(l);
     initialStates.push(state);
     radius.rotate(floor(360 / numberOfLights));
@@ -33,17 +35,19 @@ function draw() {
   ellipse(width / 2, height / 2, 30, 30);
   ellipse(width / 2, height / 2, 50, 50);
 
-
   for (let i = 0; i < numberOfLights; i++) {
     lights[i].show();
   }
 }
 
 function mousePressed() {
-  let m = createVector(mouseX,mouseY);
-
-  for (let i = 0; i < numberOfLights; i++) {
-    lights[i].update(m);
+  let clickFrameCount = frameCount;
+  if (frameCount - lastClickFrameCOount > 15) {
+    lastClickFrameCOount = clickFrameCount;
+    let m = createVector(mouseX, mouseY);
+    for (let i = 0; i < numberOfLights; i++) {
+      lights[i].update(m);
+    }
   }
 }
 
@@ -73,22 +77,19 @@ class Light {
     if (d <= 30) {
       // this.state = !this.state;
 
-      let ind = this.number-1;
-      let prev = ind-1;
-      let next = ind+1;
-      if(prev<0)
-      {
-        prev = numberOfLights-1;
+      let ind = this.number - 1;
+      let prev = ind - 1;
+      let next = ind + 1;
+      if (prev < 0) {
+        prev = numberOfLights - 1;
       }
 
-      if(next >= numberOfLights)
-      {
+      if (next >= numberOfLights) {
         next = 0;
       }
 
       lights[prev].state = !lights[prev].state;
       lights[next].state = !lights[next].state;
-
     }
   }
 }
