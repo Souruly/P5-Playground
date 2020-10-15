d71faf3bccfe31c6dc68fd64d077e190545e4530
Sourav Kulkarni <souruly@gmail.com>
2020-10-15 22:31:48 +0530

    Added new sketch

--- sketch.js
+++ sketch.js
@@ -1,122 +1,99 @@
-let numberOfLights = 8;
-let lights = [];
-let states = [true, false];
-let center;
-let initialStates = [];
-let lastClickFrameCount = 0;
-let playButton;
-let numberSlider;
-let gameState = 0;
+let stripes = [];
+let numberOfStripes = 25;
+let t1 = 15;
+let t2 = 5;
+let img;
+let stripeSpeed = 1;
+let hidden = false;
+let showButton;
 
-function generateGame() {
-  lights = [];
-  initialStates = [];
+let imageFile = "assets/composite.jpg"
 
-  numberOfLights = numberSlider.value();
-
-  let radius = createVector(200, 0);
-  for (let i = 0; i < numberOfLights; i++) {
-    let position = p5.Vector.add(center, radius);
-    let state = random(states);
-    let l = new Light(position.x, position.y, state, i + 1);
-    lights.push(l);
-    initialStates.push(state);
-    radius.rotate(floor(360 / numberOfLights));
-  }
-
-  console.log(initialStates);
-}
-
-function startPuzzle() {
-  gameState = 1;
-  playButton.hide();
-  numberSlider.hide();
+function preload() {
+  img = loadImage(imageFile);
 }
 
 function setup() {
-  createCanvas(600, 600);
+  myCanvas = createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
-  ellipseMode(RADIUS);
   rectMode(CENTER);
-  textAlign(CENTER, CENTER);
-  center = createVector(300, 300);
+  imageMode(CENTER);
+  textSize(16);
 
-  numberSlider = createSlider(3, 20, 8);
-  numberSlider.position(400, 50);
-  numberSlider.changed(generateGame);
+  for (let i = 0; i < numberOfStripes; i++) {
+    let stripeX = i * (t1 + t2);
+    let s = new Stripe(stripeX);
+    stripes.push(s);
+  }
 
-  playButton = createButton("Start");
-  playButton.position(450, 500);
-  playButton.mousePressed(startPuzzle);
-
-  generateGame();
+  showButton = createButton("Show Stripes");
+  if(hidden)
+  {
+    showButton.html("Show Stripes")
+  }
+  else
+  {
+    showButton.html("Hide Stripes")
+  }
+  showButton.mousePressed(toggleShow);
+  showButton.position(width-250,100);
 }
 
 function draw() {
-  background(240);
-
-  for (let i = 0; i < numberOfLights; i++) {
-    lights[i].show();
-  }
-
-  textSize(20);
-  text("+", width/2, height/2);
-  text("Lights : " + str(numberSlider.value()), 450, 40);
-}
-
-function mousePressed() {
-  let clickFrameCount = frameCount;
-  if (frameCount - lastClickFrameCount > 15) {
-    lastClickFrameCount = clickFrameCount;
-    let m = createVector(mouseX, mouseY);
-    for (let i = 0; i < numberOfLights; i++) {
-      lights[i].update(m);
+  background(255);
+  
+  image(img, width/2, height/2);
+  
+  stripes.forEach((s) => {
+    if (hidden == false) {
+      s.show();
     }
+    if (keyIsPressed === true) {
+      if (keyCode === UP_ARROW) {
+        s.update(1);
+      } else if (keyCode === DOWN_ARROW) {
+        s.update(-1);
+      }
+    }
+  });
+
+  noStroke();
+  fill(0);
+  text("Press UP/DOWN Arrow to move stripes.", width-200,50,200,50);
+}
+
+function toggleShow()
+{
+  hidden = !hidden;
+  if(hidden)
+  {
+    showButton.html("Show Stripes")
+  }
+  else
+  {
+    showButton.html("Hide Stripes")
   }
 }
 
-class Light {
-  constructor(x, y, state, n) {
-    this.position = createVector(x, y);
-    this.state = state;
-    this.number = n;
-    this.radius = 10;
+class Stripe {
+  constructor(x) {
+    this.x = x;
+    this.speed = 0.5;
   }
 
   show() {
-    if (this.state == true) {
-      fill(255, 255, 0);
-    } else {
-      fill(51);
-    }
-    stroke(0);
-    ellipse(this.position.x, this.position.y, this.radius, this.radius);
+    fill(192);
     noStroke();
-    fill(0);
-    textSize(15);
-    text(this.number, this.position.x + this.radius + 10, this.position.y);
+    rect(this.x, height/2, t1, height);
   }
 
-  update(m) {
-    let d = p5.Vector.dist(this.position, m);
-    if (d <= this.radius) {
-      if (gameState == 0) {
-        this.state = !this.state;
-      } else {
-        let ind = this.number - 1;
-        let prev = ind - 1;
-        let next = ind + 1;
-        if (prev < 0) {
-          prev = numberOfLights - 1;
-        }
-
-        if (next >= numberOfLights) {
-          next = 0;
-        }
-
-        lights[prev].state = !lights[prev].state;
-        lights[next].state = !lights[next].state;
-      }
+  update(s) {
+    this.x += s*this.speed;
+    if (this.x > width+t1/2) {
+      this.x = -t1/2;
+    }
+    if (this.x < -t1/2) {
+      this.x = width+t1/2;
     }
   }
 }
