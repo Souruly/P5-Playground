94d4eb2f402cfddf2dda9ae343e83c83779fdcd9
Sourav Kulkarni <souruly@gmail.com>
2022-01-07 10:35:11 +0100

    Update sketch.js

--- Generative_Art/sketch.js
+++ Generative_Art/sketch.js
@@ -1,5 +1,6 @@
 let drawButton;
 let resetButton;
+let hideLinesButton;
 let link;
 let drawing = false;
 let numberOfSegments = 3;
@@ -7,14 +8,15 @@ let segments = [];
 let angles = [];
 let points = [];
 let buffer;
-let px,py,tx,ty;
+let px, py, tx, ty;
 let hu = 0;
+let linesHidden = false;
 
-function setup(){
-  numberOfSegments = round(random(1,6));
-  createCanvas(1200,600);
+function setup() {
+  numberOfSegments = round(random(1, 6));
+  createCanvas(1200, 600);
   ellipseMode(RADIUS);
-  textAlign(CENTER,TOP);
+  textAlign(CENTER, TOP);
   rectMode(CORNERS);
   angleMode(DEGREES);
   background(240);
@@ -22,34 +24,33 @@ function setup(){
   // hu = round(random(0,360));
   hu = 255;
   drawButton = createButton("DRAW!");
-  drawButton.position(870,230);
+  drawButton.position(870, 230);
   drawButton.mousePressed(changeView);
   resetButton = createButton("Refresh");
-  resetButton.position(870,330);
+  resetButton.position(870, 330);
   resetButton.mousePressed(resetSketch);
-  let l = floor(280/numberOfSegments);
-  let seg0 = new Segment(300,300,l,0,0);
+  hideLinesButton = createButton("Toggle Line Display");
+  hideLinesButton.position(840, 260);
+  hideLinesButton.mousePressed(hideLines);
+  let l = floor(280 / numberOfSegments);
+  let seg0 = new Segment(300, 300, l, 0, 0);
   seg0.getInitialEndPoint();
   segments.push(seg0);
-  for(let i=1 ; i<numberOfSegments ; i++)
-  {
-    let parent = segments[i-1];
-    let seg = new Segment(parent.endPoint.x,parent.endPoint.y,l,0,i);
+  for (let i = 1; i < numberOfSegments; i++) {
+    let parent = segments[i - 1];
+    let seg = new Segment(parent.endPoint.x, parent.endPoint.y, l, 0, i);
     seg.getInitialEndPoint();
     segments.push(seg);
   }
 
-  for(let i=0 ; i<numberOfSegments ; i++)
-  {
-    let a = round(random(-6,6));
-    if(i==0)
-    {
+  for (let i = 0; i < numberOfSegments; i++) {
+    let a = round(random(-6, 6));
+    if (i == 0) {
       let da;
-      do{
-        da = round(random(-5,5));
-      }
-      while(da==0);
-      da = da/100;
+      do {
+        da = round(random(-5, 5));
+      } while (da == 0);
+      da = da / 100;
       a += da;
     }
     angles.push(a);
@@ -58,11 +59,15 @@ function setup(){
   buffer = createGraphics(600, 600);
   buffer.background(240);
 
-  link = createA('https://github.com/Souruly/souruly.github.io/blob/master/README.md','GITHUB REPO','_blank');
-  link.position(850,490);
+  link = createA(
+    "https://github.com/Souruly/souruly.github.io/blob/master/README.md",
+    "GITHUB REPO",
+    "_blank"
+  );
+  link.position(850, 490);
 }
 
-function draw(){
+function draw() {
   background(240);
   imageMode(CORNER);
   image(buffer, 0, 0, 600, 600);
@@ -74,32 +79,31 @@ function draw(){
   noStroke();
   textSize(40);
   textStyle(BOLD);
-  text("GENERATIVE ART",900,0);
+  text("GENERATIVE ART", 900, 0);
   textStyle(NORMAL);
   textSize(18);
-  //text("A LOW RES. SIMULATION FOR STATIC CHARGE STRUCTURE",900,42);
-  text("Number of segment(s) : " + numberOfSegments,900,100);
-  text("Rotation(s) : " + angles,900,150);
-  text("Press this button to start drawing.",900,200);
-  text("Refresh the page to get new drawings each time.",900,300);
+  text("Number of segment(s) : " + numberOfSegments, 900, 100);
+  text("Rotation(s) : " + angles, 900, 150);
+  text("Press this button to start drawing.", 900, 200);
+  text("Refresh the page to get new drawings each time.", 900, 300);
   stroke(0);
   //line(900,0,900,600);
   strokeWeight(5);
-  line(600,70,1200,70);
+  line(600, 70, 1200, 70);
   strokeWeight(3);
-  line(600,450,1200,450);
+  line(600, 450, 1200, 450);
 
-  for(let i=0 ; i<numberOfSegments ; i++)
-  {
-    segments[i].show();
+  if (!linesHidden) {
+    for (let i = 0; i < numberOfSegments; i++) {
+      segments[i].show();
+    }
   }
 
   let l = segments.length;
-  tx = segments[l-1].endPoint.x;
-  ty = segments[l-1].endPoint.y;
+  tx = segments[l - 1].endPoint.x;
+  ty = segments[l - 1].endPoint.y;
 
-  if(drawing)
-  {
+  if (drawing) {
     buffer.stroke(0);
     // colorMode(HSB);
     // let c = color(hu, 0, 0);
@@ -116,34 +120,33 @@ function draw(){
     // }
   }
 
-  for(let i=0 ; i<numberOfSegments ; i++)
-  {
-    segments[i].update(angles[i],segments);
+  for (let i = 0; i < numberOfSegments; i++) {
+    segments[i].update(angles[i], segments);
   }
   drawBorders();
   px = tx;
   py = ty;
 }
 
-function changeView()
-{
-  if(drawing==false)
-  {
+function changeView() {
+  if (drawing == false) {
     drawing = true;
   }
 }
 
-function resetSketch()
-{
+function resetSketch() {
   window.location.reload();
 }
 
-function drawBorders()
-{
+function drawBorders() {
   stroke(0);
   strokeWeight(5);
-  line(1,1,600-1,1);
-  line(600-1,1,600-1,height-1);
-  line(600-1,height-1,1,height-1);
-  line(1,height-1,1,1);
+  line(1, 1, 600 - 1, 1);
+  line(600 - 1, 1, 600 - 1, height - 1);
+  line(600 - 1, height - 1, 1, height - 1);
+  line(1, height - 1, 1, 1);
+}
+
+function hideLines() {
+  linesHidden = !linesHidden;
 }
